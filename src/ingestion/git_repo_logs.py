56b48c3ffd5f7f2c6711889163e64dbd17527e32
Guillaume DELEPOULLE <guillaume.delepoulle.ext@sevetys.fr>
2024-04-08 16:07:06 +0200

    fix(ingestion/github_repo): correct ressource and load token with .env

--- src/ingestion/git_repo_logs.py
+++ src/ingestion/git_repo_logs.py
@@ -1,17 +1,49 @@
+import os
+from collections.abc import Generator
+from typing import Any
+
 import dlt
-import requests
+from dlt.sources.helpers import requests
+from dotenv import load_dotenv
 
-url = "https://api.github.com/repos/dlt-hub/dlt/issues"
-# Make a request and check if it was successful
-response = requests.get(url)
-response.raise_for_status()
 
-pipeline = dlt.pipeline(
-    pipeline_name="github_issues",
-    destination="duckdb",
-    dataset_name="github_data",
+@dlt.resource(
+    table_name="commits",
+    write_disposition="merge",
+    primary_key="id",
 )
-# The response contains a list of issues
-load_info = pipeline.run(response.json(), table_name="issues")
+def get_commits() -> Generator[dict[str, Any], None, None]:
+    github_token = os.environ.get("GITHUB_REPO_TOKEN")
+    repository = "gdelepoulle/local-efficient-data-platform"
 
+    # GitHub API URL for fetching commits from the specified repository
+    url = f"https://api.github.com/repos/{repository}/commits"
+
+    headers = {
+        "Authorization": f"token {github_token}",
+        "Accept": "application/vnd.github.v3+json",
+    }
+
+    while True:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        yield response.json()
+
+        # Get next page
+        if "next" not in response.links:
+            break
+        url = response.links["next"]["url"]
+
+
+load_dotenv()
+pipeline = dlt.pipeline(
+    pipeline_name="github_commits_pipeline",
+    destination="duckdb",
+    dataset_name="github_commits",
+)
+load_info = pipeline.run(get_commits)
+row_counts = pipeline.last_trace.last_normalize_info
+
+print(row_counts)
+print("------")
 print(load_info)
